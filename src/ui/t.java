b19d92a5c40f1d8b5e9d02383b7413406f939423
Bruce Li <isanoob1897@outlook.com>
2024-05-20 19:30:15 -0700

    add

---
+++ src/ui/t.java
@@ -0,0 +1,13 @@
+package ui;
+import javax.swing.*;
+import java.awt.*;
+
+
+public class t {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Tipper UI");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 400);
+        frame.setVisible(true);        
+    }
+}
