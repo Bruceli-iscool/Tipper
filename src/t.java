a08410be42bbae26e208ebeb25870f4f752cfe31
Bruce Li <isanoob1897@outlook.com>
2024-05-20 18:49:26 -0700

    add

--- src/t.java
+++ src/t.java
@@ -3,14 +3,32 @@ import java.util.Scanner;
 public class t 
 {
     public static void main(String args[]) 
-    {
-        if (args.length < 1) 
+    { 
+        try 
         {
-            Tipper t = new Tipper();
-            System.out.println(t.calculate());
-        }  
-        else if (args.length > 1) {
-            
+            if (args.length < 1) 
+            {
+                Tipper t = new Tipper();
+                System.out.println(t.calculate());
+            }  
+            else if (args.length > 1) 
+            {
+                double z = Double.parseDouble(args[0]);
+                double y = Double.parseDouble(args[1]);
+                Tipper t = new Tipper(z, y);
+                System.out.println(t.calculate());
+            } 
+            else
+            {
+                    System.out.println("    Tipper Copyright (C) 2024  Bruce Li\r\n" + //
+                                                "    This program comes with ABSOLUTELY NO WARRANTY;\r\n" + //
+                                                "    This is free software, and you are welcome to redistribute it\r\n" + //
+                                                "    under certain conditions;");
+            }
+
+        } catch (Exception e) 
+        {
+            System.err.println(e);
         }
     };
 };
